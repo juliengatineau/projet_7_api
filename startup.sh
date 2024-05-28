4c3abe3a425001da540fc5d815228b8c0a125051
juliengatineau <julien_gatineau@hotmail.com>
2024-05-28 17:45:32 +0200

    encore

--- startup.sh
+++ startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # Lancer le backend
+print("-----------------------------------------------------------------------)
+print("Lancement du backend")
+source antenv/bin/activate
+print("-----------------------------------------------------------------------)
+print("Antenv activé")
 gunicorn --bind=0.0.0.0:$PORT app:app
