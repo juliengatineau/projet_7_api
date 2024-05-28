022c568395ffc29dedf7cf7239af9b2f818e9ab3
juliengatineau <julien_gatineau@hotmail.com>
2024-05-28 18:13:00 +0200

    ivfvn

--- startup.sh
+++ startup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Lancer le backend
+echo "-------------------------------------------------------------"
+echo "Lancement du backend"
 source antenv/bin/activate
-print("-----------------------------------------------------------------------")
+echo "-------------------------------------------------------------"
+echo "Antenv active"
 print("Antenv active")
 gunicorn --bind=0.0.0.0:$PORT app:app
